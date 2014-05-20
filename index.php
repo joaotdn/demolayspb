0d4767ee8c7def97ed0680489f2d99d1ba1b66ed
Joao Teodoro <joaotdn@gmail.com>
2014-05-20 11:54:26 -0300

    Init project

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ordem Demolay | GCEPB</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.1/modernizr.min.js"></script>
+  </head>
+  <body>
+    
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="scripts.js"></script>
+  </body>
+</html>
