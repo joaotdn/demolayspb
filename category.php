5edbd98a319c12483275fcd0cd95f44062ea9609
Joao Teodoro <joaotdn@gmail.com>
2014-08-09 19:12:57 -0300

    Iniciando tema wordpress

--- category.php
+++ category.php
@@ -291,6 +291,18 @@
             </li>
           </ul>
         </nav>
+
+        <ul class="pagination">
+          <li class="arrow unavailable"><a href="">&laquo;</a></li>
+          <li class="current"><a href="">1</a></li>
+          <li><a href="">2</a></li>
+          <li><a href="">3</a></li>
+          <li><a href="">4</a></li>
+          <li class="unavailable"><a href="">&hellip;</a></li>
+          <li><a href="">12</a></li>
+          <li><a href="">13</a></li>
+          <li class="arrow"><a href="">&raquo;</a></li>
+        </ul>
       </section> 
       
       <section class="hide-sidebar small-16 columns hide-for-large-up"></section><!-- //conteudo clonado -->
